a64041426e20caec9fbb8405f84382f7e72e1a28
Stephen Moore <judo2000@comcast.net>
2022-01-07 13:07:44 -0700

    Update employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,11 +1,27 @@
 const Employee = require('../lib/employee');
 
 describe('Employee', () => {
-  it('should create a new employee and return the employee info by calling getEmail, getId, and getEmail', () => {
+  it('should create an employee object', () => {
+    const employee = new Employee('Stephen', 40, 'judo2000@comcast.net');
+
+    expect(employee.name).toEqual(expect.stringContaining('Stephen'));
+  });
+
+  it("Should get the employee's name with the getName() method ", () => {
     const employee = new Employee('Stephen', 40, 'judo2000@comcast.net');
 
     expect(employee.getName()).toEqual('Stephen');
+  });
+
+  it("Should get the employee's id with the getId() method", () => {
+    const employee = new Employee('Stephen', 40, 'judo2000@comcast.net');
+
     expect(employee.getId()).toEqual(40);
+  });
+
+  it("Should get the employee's email address with the getEmail() method", () => {
+    const employee = new Employee('Stephen', 40, 'judo2000@comcast.net');
+
     expect(employee.getEmail()).toEqual('judo2000@comcast.net');
   });
 });
